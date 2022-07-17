d4b24e71ab699def36125c5727c3160bbe007b5c
Ome-Ananenu <ananenueunice@gmail.com>
2022-07-17 04:15:08 +0100

    Day_8

---
+++ kata_day_8/check_for_factor.java
@@ -0,0 +1,6 @@
+public class Kata {
+    public static boolean checkForFactor(int base, int factor) {
+        // your code here
+      return base % factor == 0;
+    }
+}
---
+++ kata_day_8/sum_of_angles.java
@@ -0,0 +1,6 @@
+public class AngleSum {
+  public static int sumOfAngles(int n) {
+    // Code here
+    return 180*(n - 2);
+  }
+}
