5582ea08dc231ef8fb3897b48b178fb2f9985ffe
Ome-Ananenu <ananenueunice@gmail.com>
2022-07-18 22:23:59 +0100

    Move some items

--- repeat_it.java
+++
@@ -1,8 +0,0 @@
-public class RepeatIt {
-
-  public static String repeatString(final Object toRepeat, final int n) {
-    
-    return toRepeat instanceof String? ((String) toRepeat).repeat(n): "Not a string";
-  }
-  
-}
