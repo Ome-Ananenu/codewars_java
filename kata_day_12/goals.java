04dfed3cd75170230aa9d398bb1cab60613ac50b
Ome-Ananenu <ananenueunice@gmail.com>
2022-07-21 23:24:48 +0100

    Initial Commmit

---
+++ kata_day_12/goals.java
@@ -0,0 +1,6 @@
+class Goals {
+  final public static int laLigaGoals = 43,
+                          championsLeagueGoals = 10,
+                          copaDelReyGoals = 5,
+                          totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+}
