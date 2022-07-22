ea0891a5d5c6f14a8e4610ba5041849855c40a03
Ome-Ananenu <ananenueunice@gmail.com>
2022-07-22 23:43:32 +0100

    Sorted

---
+++ kata_day_13/sorted.java
@@ -0,0 +1,15 @@
+class Solution {
+  
+  public static String isSortedAndHow(int[] array) {
+    boolean desc = false;
+    boolean asc = false;
+    for(int i = 1; i < array.length; i++) {
+      if(array[i] > array[i-1]) asc = true;
+      if(array[i] < array[i-1]) desc = true;
+    }
+    if(asc && !desc) return "yes, ascending";
+    if(desc && !asc) return "yes, descending";
+    return "no";
+  }
+  
+}
