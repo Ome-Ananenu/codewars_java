c929959214c8b8c3dd2d782ec6f6ab7b4382b72b
OmeAnanenu <omenebele.ananenu@teamapt.com>
2022-07-30 10:33:45 +0100

    Printer

---
+++ kata_day_18/printer.java
@@ -0,0 +1,15 @@
+public class Printer {
+    
+    public static String printerError(String s) {
+      String right = "abcdefghijklm";
+      int count = 0;
+      String ans = String.format("%s/%s",count,s.length());
+      
+      for(int i=0; i<s.length(); i++){
+        if(!right.contains(s.indexOf(i))){
+          count++;
+        }
+      }
+        // your code
+    }
+}
