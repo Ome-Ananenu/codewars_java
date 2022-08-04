810f37d573491d53c84173dbf4ac49fcc44d656c
OmeAnanenu <ananenueunice@gmail.com>
2022-08-04 22:18:09 +0100

    KataExampleTwist

--- kata_day_19/example_twist.java
+++ kata_day_19/example_twist.java
@@ -10,3 +10,15 @@ public class KataExampleTwist
         return websites;
     }
 }
+
+import java.util.*;
+
+public class KataExampleTwist
+{
+    public static String[] kataExampleTwist()
+    {
+        String[] websites = new String[1000];
+        Arrays.fill(websites,"codewars");
+        return websites;
+    }
+}
