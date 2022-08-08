a738df10bdead094df169e1d25646653bfafc69d
OmeAnanenu <ananenueunice@gmail.com>
2022-08-08 20:08:39 +0100

    fix city welcome

--- kata_day_20/city_welcome.java
+++ kata_day_20/city_welcome.java
@@ -1,7 +1,16 @@
 public class Hello{
   public String sayHello(String [] name, String city, String state){
     //code here
-    String spa = " ";
-    return String.format("Hello, %s! Welcome to %s, %s!",name[0]+spa+name[1],city,state);
+    String spa = "";
+    for(int i=0; i<name.length; i++){
+      if(i != name.length -1){
+         spa += name[i] + " ";
+      }
+     else{
+        spa += name[i];
+     }
+    }
+    
+    return String.format("Hello, %s! Welcome to %s, %s!",spa,city,state);
   }
 }
