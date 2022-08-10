a5ae0dff88d9654c85df4c814d98e1d776777824
OmeAnanenu <ananenueunice@gmail.com>
2022-08-10 08:30:55 +0100

    XOR operartor

---
+++ kata_day_21/xorr.java
@@ -0,0 +1,7 @@
+public class XOR {
+    
+    public static boolean xor(boolean a, boolean b) {
+        //your code here:
+        return (a == true && b == false) || (a == false && b == true)?true:false;
+    }
+}
