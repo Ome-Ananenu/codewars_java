20f1c2bd7a1aa3422f41211058d15b952db7b56a
OmeAnanenu <ananenueunice@gmail.com>
2022-08-11 21:19:14 +0100

    Oscars

--- kata_day_21/oscars.java
+++ kata_day_21/oscars.java
@@ -16,3 +16,12 @@ public class Kata
     }
   }
 }
+
+public class Kata{
+  public static String leo(final int oscar) {
+    if (oscar == 88) return "Leo finally won the oscar! Leo is happy";
+    if (oscar == 86) return "Not even for Wolf of wallstreet?!";
+    if (oscar < 88) return "When will you give Leo an Oscar?";
+    return "Leo got one already!";
+  }
+}
