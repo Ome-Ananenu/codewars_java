db0edb5248c05cf38a9b10cde9772a5142f8b723
OmeAnanenu <ananenueunice@gmail.com>
2022-08-14 10:21:26 +0100

    Sum of cubes

---
+++ kata_day_22/sum_of_cubes.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+
+public class Solution{
+  public static long sumCubes(long n){
+    long ans = 0;
+    
+    for(long i=1; i<= n; i++){
+      ans += Math.pow(i,3);
+    }
+    return ans;
+  }
+}
