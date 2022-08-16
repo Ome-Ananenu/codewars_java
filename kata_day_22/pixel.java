e6a29b529c11d6a4ade9d89fd3546d5bec2e96f6
OmeAnanenu <ananenueunice@gmail.com>
2022-08-16 20:08:06 +0100

    alt

--- kata_day_22/pixel.java
+++ kata_day_22/pixel.java
@@ -13,3 +13,10 @@ public class Kata {
   }
 
 }
+public class Kata {
+
+  public static boolean isDivisible(int wallLength, int pixelSize) {
+    return pixelSize == 0? false: wallLength % pixelSize == 0;
+  }
+
+}
