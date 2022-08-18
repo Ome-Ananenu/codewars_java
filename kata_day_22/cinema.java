5786260c1bf3ba3f3738b87f1b2ca295f9440d3f
OmeAnanenu <ananenueunice@gmail.com>
2022-08-18 21:13:42 +0100

    movie

--- kata_day_22/cinema.java
+++ kata_day_22/cinema.java
@@ -9,3 +9,18 @@ public class Movie {
         return count;
     }
 }
+
+public class Movie {
+
+    public static int movie(int card, int ticket, double perc) {
+        double systemA = 0, systemB = card, tempTicket = ticket;
+        int numberOfVisits = 0;
+        do {
+            systemA += ticket;
+            tempTicket *= perc;
+            systemB += tempTicket;
+            numberOfVisits++;
+        } while (systemA <= Math.ceil(systemB));
+
+        return numberOfVisits;
+    
